f2f0e36b99e459ac1063409fcdf67ab0977d5c94
samwisemurphy <Sam@Pauls-iMac.local>
2014-08-05 09:11:29 +0100

    AddFiles

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="CV.css"/>
+	</head>
+	<body>
+			<div id="header"></div>
+			<div class="left"></div>
+			<div class="right"></div>
+			<div id="footer"></div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css.map"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css.map"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		
+	</head>
+	<body>
+		
+		<div class="navbar inline">
+			<div class="container">
+				<ul class="nav nav-pills pull-left">
+					<li><a href="#">Favourr</a></li>
+					<li><a href="#">Home</a></li>				
+				</ul>
+				<ul class="nav nav-pills pull-right">	
+					<li><a href="favourrs.html">Favourrs</a></li>
+					<li><a href="#">About Us</a></li>					
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</div><!---container ending-->
+		</div><!---Nav ending-->	
+		<div class="jumbotron birds">
+			<div class="container">
+				<h3>Favourr</h3>
+				<p>Get helping in your community</p>
+			</div><!---container ending-->
+		</div><!---jumbotron ending-->							
+		<div class="about top">
+			<div class="container">	
+				<div class="row">
+					<h3>What we're all about</h3>
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img class="height-250" src="images/northern.jpg" alt="Ohh shit">
+						</div>
+						
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+					</div> 
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img class="height-250" src="images/never.png" alt="Ohh shit">
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+					</div>
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img class="height-250" src="images/clouds.jpg">
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p> 
+					</div>
+				</div><!---row ending-->
+			</div><!---container ending-->
+		</div><!---about ending-->
+		<div class="how top test">
+			<div class="container">
+				<div class="row">
+					<h3>How it work's</h3>			
+					<div class="col-md-4">
+					<div class="thumbnail">
+							<img class="height-250" src="images/sign.jpg" alt="Ohh shit">
+						</div>
+						
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+					</div> 
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img class="height-250" src="images/garden.jpeg" alt="Ohh shit">
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+					</div>
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img class="height-250" src="images/balloons.jpg">
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p> 
+					</div>
+				</div><!---row ending-->
+			</div><!---container ending-->
+		</div><!---how ending-->
+		<div class="jumbotron bottom top">
+			<div class="container top">
+				<h3>Get involved</h3>
+				<p>
+					<a class ="btn btn-lg btn-success" href="#" role="button">Take the leap</a>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
+
+<script src="js/jquery.min.js" type="text/javascript"></script>  
+<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>  
